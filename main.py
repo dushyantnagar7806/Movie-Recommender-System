b5c0de149f706cfefda3e19df9608c2e2bc95855
Dushyant Nagar <dushyantnagar7806@gmail.com>
2023-07-28 17:37:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(
+            movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names,recommended_movie_posters
+
+
+movies_dict = pickle.load(open('movie_dict.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+st.header('Movie Recommender System')
+
+# selectbox
+
+selected_movie_name = st.selectbox('Select a movie:',options=movies.title.values)
+
+
+# ... (previous code)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie_name)
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(recommended_movie_names[i])
+            st.image(recommended_movie_posters[i])
+
+
